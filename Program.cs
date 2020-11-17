874d5979f95914d7fb0cb537f8debd2954566dbe
LuayRahil02 <54432462+LuayRahil02@users.noreply.github.com>
2020-11-17 10:49:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryparese.Luay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!int.TryParse(Console.ReadLine(), out resultat))
+                Console.WriteLine("Skriv en siffra");
+
+
+
+        }
+
+        public static bool TryParse(string s, out int result)
+        {
+            try
+            {
+                result = int.Parse(s);
+                return true;
+            }
+            catch
+            {
+                result = 0;
+                return false;
+            }
+
+
+
+        }
+    }
+}
